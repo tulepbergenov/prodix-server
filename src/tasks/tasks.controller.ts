086d39190515fb0c0744a1fa1a2f3d8ff6d07b03
Daulet Tulepbergenov <daulet6368@gmail.com>
2025-03-23 17:34:41 +0500

    feat: add task module

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(): Task[] {
+    return this.tasksService.getTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task | undefined {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task | undefined {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask({ title, description }: CreateTaskDto): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  getTaskById(id: string): Task | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task | undefined {
+    const task = this.getTaskById(id);
+
+    if (task) {
+      task.status = status;
+    }
+
+    return task;
+  }
+}
